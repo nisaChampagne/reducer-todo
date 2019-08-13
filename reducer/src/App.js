f632231c788b7ebb833bd273e5cf4b1e65452912
nisaChampagne <50988313+nisaChampagne@users.noreply.github.com>
2019-08-12 21:16:20 -0400

    getting some of what I can do set up

--- reducer/src/App.js
+++ reducer/src/App.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
+import TodoList from './components/TodoList';
+import ListForm from './components/ListForm';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className='app'>
+      <div className='header'>
+        <h1>Shopping List</h1>
+        <ListForm />
+      </div>
+      <TodoList />
     </div>
   );
 }
