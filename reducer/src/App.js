b6a922f6cee43801f83fc8e315323bbfa0e47504
nisaChampagne <50988313+nisaChampagne@users.noreply.github.com>
2019-08-13 09:42:25 -0400

    added my useReducer

--- reducer/src/App.js
+++ reducer/src/App.js
@@ -1,9 +1,11 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
 import TodoList from './components/TodoList';
 import ListForm from './components/ListForm';
 import './App.css';
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState)
+  
   return (
     <div className='app'>
       <div className='header'>
